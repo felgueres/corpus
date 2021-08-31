37ce31413b109d01c156044574fa525f5f09604f
pablo <pablofelgueres@gmail.com>
2021-08-30 22:24:14 -0700

    wip - nside bar for app look

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import { useAuth0 } from "@auth0/auth0-react";
 import { NavBar, Loading, PrivateRoute } from "./components";
-import { Home, Profiles, Profile} from "./views";
+import { Home, Profiles, Profile } from "./views";
 import "./app.css";
 
 const App = () => {
@@ -15,16 +15,33 @@ const App = () => {
 
   return (
     <div id="app" className="d-flex flex-column h-100">
-      <NavBar />
-      <Container className="flex-grow-1 mt-1">
-        <Switch>
-          <Route path="/profiles" exact component={Profiles} />
-          <Route path="/profiles/:organizationId" exact component={Profile} />
-          <Route path="/" exact component={Home} />
-        </Switch>
-      </Container>
+      {/* <NavBar /> */}
+      <Row className="h-100">
+        <Col className="col-2 bg-white">
+          <ul class="nav flex-column mb-auto">
+            <br></br>
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page">
+                Introduction 
+              </a>
+            </li>
+            <li>
+              <a class="nav-link link-dark">
+                Search Companies
+              </a>
+            </li>
+          </ul>
+      </Col>
+          <Col className="col-10 bg-light">
+            <Switch>
+              <Route path="/profiles" exact component={Profiles} />
+              <Route path="/profiles/:organizationId" exact component={Profile} />
+              <Route path="/" exact component={Home} />
+            </Switch>
+          </Col>
+      </Row>
     </div>
-  );
+      );
 };
 
-export default App;
+      export default App;
