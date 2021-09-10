20accd2bb8260311c5c1cca7701d1a390d3cf4c8
pablo <pablofelgueres@gmail.com>
2021-09-10 09:09:03 -0700

    wip add db search call

--- src/views/profiles.js
+++ src/views/profiles.js
@@ -3,9 +3,29 @@ import { Col, Row } from "react-bootstrap";
 import { ProfilesTable, SideBar, ProfileSummary } from "../components";
 import { Route, Switch } from "react-router-dom";
 
-export const Profiles = ({match}) => {
+export const Profiles = ({ match }) => {
 
   const [searchTerm, setSearchTerm] = useState(null);
+  const [searchResult, setSearchResult] = useState(null);
+
+  const apiUrl = process.env.REACT_APP_API_URL;
+  const fetchSearchResults = async (data) => {
+    try {
+      const response = await fetch(`${apiUrl}/api/organizations/search`,  
+        { method: 'PUT',
+          body: JSON.stringify(data),
+          headers: {'Content-Type': 'application/json'}});
+      const responseData = await response.json();
+      setSearchResult(responseData);
+    } catch (error) {
+      setSearchResult(error.message);
+    }
+  };
+
+  function handleSearch(event) {
+    setSearchTerm(event.target.value)
+    fetchSearchResults(searchTerm).then(console.log(searchResult))
+  }
 
   return (
     <Row className='vh-100'>
@@ -13,7 +33,7 @@ export const Profiles = ({match}) => {
         <SideBar />
       </Col>
       <Col className="not-sidebar pt-5">
-        <input id='searchbar' type="text" className="form-control searchbar" autoComplete="off" placeholder="Search" onChange={event => {setSearchTerm(event.target.value)}} />
+        <input id='searchbar' type="text" className="form-control searchbar" autoComplete="off" placeholder="Search" onChange={event => { handleSearch(event) }} />
         <div id='blurable' className='mb-5'>
           <Switch>
             <Route path={`${match.path}`} exact component={ProfilesTable} />
