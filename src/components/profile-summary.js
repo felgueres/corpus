aa6cc307103f8043d2f834fb4351f0cadc2db294
pablo <pablofelgueres@gmail.com>
2021-09-25 16:48:05 -0700

    add cards for stats

--- src/components/profile-summary.js
+++ src/components/profile-summary.js
@@ -22,7 +22,7 @@ const ProfileSummary = ({ match }) => {
   }
   var data = organizationInformation[0];
   const matchCounter = (text, regex) => {
-    if (text.toLowerCase().match(regex)){
+    if (text.toLowerCase().match(regex)) {
       return (text.toLowerCase().match(regex).length)
     } else {
       return 0
@@ -33,6 +33,8 @@ const ProfileSummary = ({ match }) => {
     var climateMention = 0
     var ghgMention = 0
     var climateRiskMention = 0
+    var weatherMention = 0
+    var materialMention = 0
 
     for (var i = 0; i < textArr.length; i++) {
       var line = textArr[i];
@@ -40,48 +42,53 @@ const ProfileSummary = ({ match }) => {
       ghgMention += matchCounter(line, /\bghg\b/g)
       ghgMention += matchCounter(line, /\bgreenhouse\b/g)
       climateRiskMention += matchCounter(line, /\bclimate risk\b/g)
-     }
+      weatherMention += matchCounter(line, /\bextreme weather\b/g)
+      weatherMention += matchCounter(line, /\bweather\b/g)
+      materialMention += matchCounter(line, /\bmaterial impact\b/g)
+    }
 
     return (
       <div>
         Climate risk: {climateRiskMention}
-        <br/>
-        Climate change: {climateMention} 
-        <br/>
+        <br />
+        Climate change: {climateMention}
+        <br />
         Greenhouse gasses: {ghgMention}
+        <br />
+        Material impact: {materialMention}
+        <br />
+        Severe weather: {weatherMention}
       </div>
     )
   }
 
   const renderDisclosure = (idx, line) => {
     return (
-    <tr key={idx}>
-      <p>
-        {line}
-      </p>
-    </tr>)
+      <tr key={idx}>
+        <p className="profile-snippet">
+          {line}
+        </p>
+      </tr>)
   }
 
   return (
     <div className="my-5">
       <h3 className="profile-title">{organizationId}</h3>
-      <Row className='py-2' style={{ 'marginLeft': '0px' }}>
-        <Col className="investor-disclosure border shadow col-md-8 col-sm-12 ">
-          <div className="my-3 profile-snippet">
-            <p className="my-2 profile-findings"><strong>Annual Investor Report (10K), 2021 </strong></p>
+      <Row className='py-2'>
+        <Col sm={12} md={8}>
+          <div className="investor-disclosure border profile-cards ">
+            <p className="my-3 profile-findings"><strong>Annual Investor Report (10K), 2021 </strong></p>
             <p className="profile-findings">Climate-related snippets</p>
             {Object.entries(data.risks).map(([idx, line],) => renderDisclosure(idx, line))}
           </div>
         </Col>
-        <Col className="investor-disclosure">
-          <div className="profile-findings">
-            <p className="my-1 profile-findings"><strong> Keyword stats </strong></p>
-            <p className="my-1">{getStats(data.risks)}</p>
+        <Col sm={12} md={4}>
+          <div className="investor-disclosure border profile-cards">
+            <p className="my-3 profile-findings"><strong> Keywords </strong></p>
+            <p className="my-3 profile-findings">{getStats(data.risks)}</p>
           </div>
-          <p className="my-1"></p>
         </Col>
       </Row>
-
     </div>
   );
 };
