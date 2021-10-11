1d77740bb53dbee28adce449f7d1397367d08e01
pablo <pablofelgueres@gmail.com>
2021-10-11 09:11:07 -0700

    add better SEO

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ const Footer = () => (
   <div>
     <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-5 border-top bg-white">
       <div className="col-md-11 d-flex align-items-center">
-        <span><strong className="navbar-font">ClimateDisclosures</strong> helps investors quickly screen climate-related disclosures in 10K filings.</span>
+        <span><strong className="navbar-font">ClimateDisclosures</strong> helps investors quickly screen climate-related financial disclosures.</span>
       </div>
       <ul className="col-md-1 justify-content-end list-unstyled d-flex">
         <li className="px-3"><a href="https://twitter.com/getclimatecap"><BsTwitter/></a></li>
