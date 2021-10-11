8caa68daf834e21a699c705aa1937a02189b1779
pablo <pablofelgueres@gmail.com>
2021-10-10 17:54:03 -0700

    add footer

--- src/app.js
+++ src/app.js
@@ -20,6 +20,7 @@ const App = () => {
           <Route path="/" component={Profiles} />
         </Switch>
       </div>
+      <Footer/>
     </div>
   );
 };
--- src/components/footer.js
+++ src/components/footer.js
@@ -2,11 +2,14 @@ import React from "react";
 import { Row } from "react-bootstrap";
 
 const Footer = () => (
-  <footer className="footer bg-white text-center border footer shadow-sm">
-    <p>
-      Climate Cap 2021
-    </p>
-  </footer>
+  <div>
+    <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-5 border-top bg-white">
+      <div className="col-md-8 d-flex align-items-center">
+        <span><strong className="navbar-font">ClimateCap</strong> helps investors screen companies by climate risk and opportunities.</span>
+      </div>
+
+    </footer>
+  </div>
 );
 
 export default Footer;
