0afa8b19ed06d5907f7f4ae36e1784c1d0518845
pablo <pablofelgueres@gmail.com>
2021-10-22 14:06:23 -0700

    add tooltips

--- src/components/profiles-table.js
+++ src/components/profiles-table.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { Spinner } from "react-bootstrap";
+import { Card, Spinner } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { Col, Row } from "react-bootstrap";
+import { Col, Row, Tooltip, OverlayTrigger, Button, Image } from "react-bootstrap";
 
 const ProfilesTable = () => {
   const [cardsInformation, setCardsInformation] = useState(null);
@@ -18,20 +18,32 @@ const ProfilesTable = () => {
   };
 
   const renderRow = (idx, card) => {
-    var color = card.has_disclosures ? "#90EE90" : "#FF7F7F"
-    console.log(color)
+    var color = card.has_disclosures ? "#90EE90" : "#D3D3D3"
     return (
       <Link className='profile-card' to={`/profiles/${card.company_name}`}>
-      <Row className='my-3 py-4 px-3 pointer border border-card d-flex justify-content-between'> 
-        <span>
-          <svg data-toggle="tooltip" data-placement="top" title="tooltop" height="18" width="18">
-            <circle cx="9" cy="9" r="3" stroke={color} stroke-width="3" fill={color} />
-          </svg>
-            <span className='button-divider'/>
-        {card.company_name}</span>
-        <span className='sm-font'>{card.has_disclosures}</span>
-      </Row>
-    </Link>
+        <Row className='my-3 py-4 px-3 pointer border border-card d-flex justify-content-between'>
+          <span>
+            <OverlayTrigger
+              placement="bottom"
+              overlay={<Tooltip id="button-tooltip-2">{card.has_disclosures ? "Has Disclosures" : "No Disclosures"}</Tooltip>}
+            >
+              {({ ref, ...triggerHandler }) => (
+                <Button
+                  variant="transparent"
+                  {...triggerHandler}
+                  className="d-inline-flex align-items-center"
+                >
+                  <svg ref={ref} height="18" width="18">
+                    <circle cx="9" cy="9" r="3" stroke={color} stroke-width="3" fill={color} />
+                  </svg>
+                </Button>
+              )}
+            </OverlayTrigger>
+            <span className='button-divider' />
+            {card.company_name}
+          </span>
+        </Row>
+      </Link >
     )
   }
 
@@ -62,8 +74,8 @@ const ProfilesTable = () => {
 
   return (
     <Col sm={12} md={6}>
-        <strong>Explore Companies</strong>
-        {buildTable(cardsInformation)}
+      <strong>Explore Companies</strong>
+      {buildTable(cardsInformation)}
     </Col>
   );
 };
