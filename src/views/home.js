415743adfb17c00bb9bf7c0fb7fced9052deb48c
pablo <pablofelgueres@gmail.com>
2022-04-28 18:39:57 -0700

    nit on copy

--- src/views/home.js
+++ src/views/home.js
@@ -6,8 +6,8 @@ export const Home = () => {
       <div id='homecopy'>
         <p><strong>American Machina</strong> sifts through annual investor reports indexing company-level information on Supply Chains, Competition, Raw Materials, Demand, Climate Risks, and more.</p>
         <p>It focuses on strategic sectors of American Industrials, especially Aerospace, Automotive, and Semiconductors, starting March 2022.</p>
-        <p>The aspirational goal is to help build a better understanding of America's wealth machine, convert insights into tractable problems, and energize people and capital toward solving these.</p>
-        <p>Inspired by American values and developed in spare time by <a href="https://twitter.com/pfelgueres">@pfelgueres</a>.</p>
+        <p>The aspirational goal is to help build a better understanding of America's wealth machine, convert insights into tractable problems, and energize people and capital toward solving them.</p>
+        <p>Inspired by American values and developed in spare time by <a href="https://twitter.com/pfelgueres">@pfelgueres</a>. If you have ideas, get in touch!</p>
       </div>
     </div>
   );
