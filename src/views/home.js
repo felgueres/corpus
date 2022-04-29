8a429fea776cae8efa0f531a74f2f7274baf22c8
pablo <pablofelgueres@gmail.com>
2022-04-28 19:00:23 -0700

    nit copy

--- src/views/home.js
+++ src/views/home.js
@@ -6,7 +6,7 @@ export const Home = () => {
       <div id='homecopy'>
         <p>This project helps sift through annual investor reports indexing company-level information on Supply Chains, Competition, Raw Materials, Demand, Operational Risks, and more.</p>
         <p>It focuses on strategic sectors of American Industrials, especially Aerospace, Automotive, and Semiconductors, starting March 2022.</p>
-        <p>The aspirational goal is to build a better understanding of America's industrial base, translate insights into tractable problems, and energize people and capital toward solving them.</p>
+        <p>The aspirational goal is to improve our understanding of America's industrial base, translate insights into tractable problems, and energize people toward solving them.</p>
         <p>Inspired by American values and developed in spare time by <a href="https://twitter.com/pfelgueres">@pfelgueres</a>. If you have ideas, get in touch!</p>
       </div>
     </div>
