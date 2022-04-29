7a4fbbdfc9f6c188110664046967f650d84c0de2
pablo <pablofelgueres@gmail.com>
2022-04-29 10:15:05 -0700

    add show more or less to insight list

--- src/components/profile-insights.js
+++ src/components/profile-insights.js
@@ -1,30 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import useInsights from "./useInsights";
 import SkeletonProfile from "../skeletons/SkeletonProfile";
+import { Button } from "react-bootstrap";
 
 function insight(i, idx) {
   return (<li key={idx}><div className={`insight insight-${i.category}`}>{i.text}</div></li>)
 }
 
-const ProfileInsights = ({ organizationId }) => {
+const ITEMSTOSHOW = 1
 
+const categoriesMap = {
+  'supply': { 'readable': 'Supply Chains' },
+  'material': { 'readable': 'Raw Materials' },
+  'manufacturing': { 'readable': 'Manufacturing' },
+  'competition': { 'readable': 'Competition' },
+  'climate': { 'readable': 'Climate Risk' },
+  'risk': { 'readable': 'Other Risks' },
+  'fact': { 'readable': 'Insights' },
+  'demand': { 'readable': 'Insights' }
+}
+
+const ProfileInsights = ({ organizationId }) => {
+  const categoriesNames = Object.entries(categoriesMap).map(([k,]) => k)
+  const initItemsToShow = Object.assign({}, ...categoriesNames.map(n => ({ [n]: { 'itemsToShow': ITEMSTOSHOW, 'expanded': false } })))
   const { insights, loading } = useInsights(organizationId)
+  const [itemsToShow, setItemsToShow] = useState({ ...initItemsToShow })
 
   if (loading) {
     return (<SkeletonProfile />)
   }
 
+  function showMore(category) { itemsToShow[category].itemsToShow === 1 ? setItemsToShow({ ...itemsToShow, [category]: { ...itemsToShow[category], 'itemsToShow': 10 } }) : setItemsToShow({ ...itemsToShow, [category]: { ...itemsToShow[category], 'itemsToShow': 1 } }) }
   function insightCards(insightsArr, category, categoryReadable) {
     return (
-      <div>
+      <div id="insight-card">
         <h4>{categoryReadable}</h4>
-        {insightsArr.filter(i => i.category === category).map((i, idx) => { return insight(i, idx) })}
+          {insightsArr.filter(i => i.category === category).slice(0, itemsToShow[category].itemsToShow).map((i, idx) => { return insight(i, idx) })}
+          {insightsArr.filter(i => i.category === category).length > 1 && (<div className="showmore"><a role="button" onClick={() => showMore(category)}>{itemsToShow[category].itemsToShow===1?'Show more':'Show less'}</a></div>)}
       </div>
     )
   }
 
-  function isCategoryAvailable(insightsArr, category){
-    return insightsArr.filter(i=> i.category === category).length > 0 
+  function isCategoryAvailable(insightsArr, category) {
+    return insightsArr.filter(i => i.category === category).length > 0
   }
 
   return (
@@ -37,7 +55,6 @@ const ProfileInsights = ({ organizationId }) => {
           {isCategoryAvailable(insights, 'competition') && insightCards(insights, 'competition', 'Competition')}
           {isCategoryAvailable(insights, 'climate') && insightCards(insights, 'climate', 'Climate Risk')}
           {isCategoryAvailable(insights, 'risk') && insightCards(insights, 'risk', 'Other risks')}
-          
         </div>
         <div id='right-col'>
           {isCategoryAvailable(insights, 'demand') && insightCards(insights, 'demand', 'Demand')}
--- src/views/home.js
+++ src/views/home.js
@@ -1,10 +1,8 @@
 import React from "react";
-import SearchBar from "../components/search-bar";
 
 export const Home = () => {
   return (
-    <div id="two-col-frame">
-
+    <div>
     </div>
   );
 };
