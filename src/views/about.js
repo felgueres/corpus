187f66b3e53ff14f4057f83917f0c369cdadec86
pablo <pablofelgueres@gmail.com>
2022-05-12 09:19:47 -0700

    copy

--- src/views/about.js
+++ src/views/about.js
@@ -5,10 +5,10 @@ export const About = () => {
     <div id="two-col-frame">
       <div id='homecopy'>
         <p>The aspirational goal is to improve our understanding of America's industrial base, translate insights into tractable problems, and energize people toward solving them.</p>
-        <p>This project sifts through SEC filings and Earnings Calls and indexes company-level information on Supply Chains, Competition, Raw Materials, Demand, Operational Risks, and more.</p>
-        <p>It focuses on strategic sectors of American Industrials, specially Aerospace, Automotive, and Semiconductors, starting March 2022.</p>
-        <p>Developed in spare time by <a href="https://twitter.com/pfelgueres">@pfelgueres</a>. 
-        <p>Get in touch pablo@americanmachina.com</p>
+        <p>This project sifts through SEC filings and Earnings Calls to index company-level information on Supply Chains, Competition, Raw Materials, Demand, Operational Risks, and more.</p>
+        <p>It focuses on strategic sectors of American Industrials; Aerospace, Automotive, and Semiconductors, starting March 2022.</p>
+        <p>Developed in spare time by <a href="https://twitter.com/pfelgueres">@pfelgueres</a>.</p>
+        <p>Have requests? Get in touch at pablo@americanmachina.com</p>
       </div>
     </div>
   );
