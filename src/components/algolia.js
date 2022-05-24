42f758dd4bd48ec55bd6de9cbbfcc41b8c273038
pablo <pablofelgueres@gmail.com>
2022-05-24 16:37:33 -0700

    add algolia as fe

--- src/components/algolia.js
+++ src/components/algolia.js
@@ -1,26 +1,41 @@
 import React from "react";
 import algoliasearch from 'algoliasearch/lite';
-import { InstantSearch, SearchBox, Hits, Highlight, Menu } from 'react-instantsearch-hooks-web';
+import { InstantSearch, SearchBox, Hits, Highlight, RefinementList } from 'react-instantsearch-hooks-web';
 
 export const AlgoliaSearch = () => {
-  const searchClient = algoliasearch('9N09TDQ25A', '5ad74991d67720596b7116b0f453c9b0');
+  const searchClient = algoliasearch(process.env.REACT_APP_ALGOLIA_ACCOUNT, process.env.REACT_APP_ALGOLIA_ID);
+  console.log(process)
 
   function Hit({ hit }) {
     console.log(hit)
     return (
       <article>
         <p>{hit.name}, {hit.role} </p>
-        <p><Highlight attribute='summary' hit={hit} /> </p>
+        <p><Highlight attribute='summary' hit={hit} />{hit.p}</p>
       </article>
     );
   }
 
   return (
-    <InstantSearch searchClient={searchClient} indexName="summary">
-      <SearchBox id='search-form-algolia'/>
-      <Menu attribute="role"/>
-      <Menu attribute="section"/>
-      <Hits hitComponent={Hit} />
+    <InstantSearch searchClient={searchClient} indexName="mvpV1">
+      <SearchBox id='search-form-algolia' />
+      <div id="two-cols-search-frame">
+        <div id='search-filters'>
+          <span className="ais-Panel-header">Company</span>
+          <RefinementList attribute="company_name" />
+          <span className="ais-Panel-header">Role</span>
+          <RefinementList attribute="role" />
+          <span className="ais-Panel-header">Section</span>
+          <RefinementList attribute="section" />
+          <span className="ais-Panel-header">Industry</span>
+          <RefinementList attribute="industry" />
+          <span className="ais-Panel-header">Keywords</span>
+          <RefinementList attribute="words" />
+        </div>
+        <div>
+          <Hits hitComponent={Hit} />
+        </div>
+      </div>
     </InstantSearch>
   );
 };
