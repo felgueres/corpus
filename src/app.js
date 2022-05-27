590641df7ef625a38c353e9216ddbec5f47b3070
pablo <pablofelgueres@gmail.com>
2022-05-27 16:46:35 -0700

    remove bootstrap

--- src/app.js
+++ src/app.js
@@ -1,18 +1,17 @@
 import "./app.css";
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import { Container } from "react-bootstrap";
 import { Home } from "./views";
 import Navbar from "./components/nav-bar";
 
 const App = () => {
   return (
-    <Container id="app">
+    <div id="app">
       <Navbar/>
         <Routes>
           <Route path="/" element={<Home/>} />
         </Routes>
-    </Container>
+    </div>
   );
 };
 
