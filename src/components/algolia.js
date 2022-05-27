2d5f5da2d933e242ffa521e50937dc28553b36fe
pablo <pablofelgueres@gmail.com>
2022-05-27 11:35:22 -0700

    remove bootstrap components

--- src/components/algolia.js
+++ src/components/algolia.js
@@ -1,60 +1,77 @@
 import React from "react";
 import algoliasearch from 'algoliasearch/lite';
-import { InstantSearch, SearchBox, InfiniteHits, RefinementList, CurrentRefinements, Highlight } from 'react-instantsearch-hooks-web';
+import { InstantSearch, SearchBox, InfiniteHits, RefinementList, Highlight } from 'react-instantsearch-hooks-web';
 
 const INDEXNAME = 'mvpV3'
+
+const sMap = { 'positive': '#7cd2af', 'negative': '#d4789d', 'neutral': '#A9A9A9' }
 
 export const AlgoliaSearch = () => {
   const searchClient = algoliasearch(process.env.REACT_APP_ALGOLIA_ACCOUNT, process.env.REACT_APP_ALGOLIA_ID);
 
   function Hit({ hit }) {
     return (
-      <article>
-        <div id='hit-title'>
-          <span>{hit.company_name} ({hit.symbol}) {'>'} {hit.name} - {hit.role}</span>
-        </div>
-        <span><Highlight attribute='s' hit={hit}/></span>
-        <div className="source">Date: {hit.period}</div>
-      </article>
+      <table style={{ 'width': '100%' }}>
+        <tr>
+          <span id="hit-title"> {hit.company_name} </span>
+          <Highlight attribute='s' hit={hit} />
+        </tr>
+        <table id='metadata'>
+          <tr>
+            <td>
+              {hit.name} {hit.role} {' | '} {`$${hit.symbol}`} {' | '} {hit.period} {' | '} {hit.industry} {' | '} {hit.s_type} {' | '} Sentiment
+              <svg height={11} width={12}><circle cx={7} cy={5} r={4} fill={sMap[hit.label]}></circle></svg>{' | '}
+              {hit.is_summary ? <span>Summary<span className="beta-pill">Beta</span></span> : 'Transcript'}
+            </td>
+          </tr>
+        </table>
+      </table >
     );
   }
 
   return (
-    <div id="search">
-      <InstantSearch searchClient={searchClient} indexName={INDEXNAME}>
-        <div id="two-col-search-frame">
-          <div>
-            <SearchBox id='search-form-algolia' placeholder={'Search companies, people, or keywords'} />
-            <div id='search-results'><InfiniteHits showPrevious={false} hitComponent={Hit} /></div>
-          </div>
-          <div>
-            <div id="search-filters">
-              <CurrentRefinements clearsQuery={true} />
-              <header>Companies</header>
-              <RefinementList operator={'or'} showMore={true} limit={15} showMoreLimit={50} sortBy={['index']} attribute="company_name" />
-              <header>Sentiment</header>
-              <RefinementList operator={'and'} attribute="label" />
-            </div>
-          </div>
-        </div>
-
-      </InstantSearch>
-    </div>
+    <InstantSearch searchClient={searchClient} indexName={INDEXNAME}>
+      <table id='search-table' style={{ 'width': '100%'}}>
+        <tr id='spacer-h' />
+        <tr><SearchBox id='search-form-algolia' placeholder={'Search companies, people, or keywords'}/></tr>
+        <tr id='spacer-h' />
+        <tr>
+          <td id='search-results'>
+            <InfiniteHits showPrevious={false} hitComponent={Hit} />
+          </td>
+          <td id='spacer-w'/>
+          <td id='search-filters'>
+            <table id='search-filters-table'>
+              <tr>
+                <td>
+                  <header>Sentiment Analysis</header>
+                  <RefinementList operator={'and'} attribute="label" />
+                  <tr id='spacer-h'/>
+                  <header>Statement Type</header>
+                  <RefinementList operator={'and'} attribute="s_type" />
+                  <tr id='spacer-h'/>
+                  <header><span>Summary<span className="beta-pill">Beta</span></span></header>
+                  <RefinementList operator={'and'} attribute="is_summary" />
+                  <tr id='spacer-h'/>
+                  <header>Industry</header>
+                  <RefinementList operator={'and'} attribute="industry" />
+                  <tr id='spacer-h'/>
+                  <header>Keywords</header>
+                  <RefinementList operator={'and'} attribute="kwords" />
+                </td>
+                <td style={{ 'width': '150px' }}>
+                  <header>Companies</header>
+                  <RefinementList operator={'and'} limit={50} sortBy={['name']} attribute="company_name" />
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+      </table>
+    </InstantSearch>
   );
 };
 export default AlgoliaSearch;
 
-        // <div id="two-col-search-frame">
-        //   <div><SearchBox id='search-form-algolia' placeholder={'Search companies, people, or keywords'} /></div>
-        //   <div><CurrentRefinements clearsQuery={true} /></div>
-        // </div>
-        // <div id="two-col-frame" className="">
-        //   <div id='search-filters' className="">
-        //     <header>Companies</header>
-        //     <RefinementList operator={'or'} showMore={true} limit={15} showMoreLimit={50} sortBy={['name']} attribute="company_name" />
-        //   </div>
-        //   <div id='search-results'>
-        //     <infinitehits showprevious={false}  />
-        //   </div>
-        // </div>
-        // <div>{hit.kwords.map(e => { return <span className="keyword-pill">{e}</span> })}</div>
+// <div>{hit.kwords.map(e => { return <span className="keyword-pill">{e}</span> })}</div>
+// <CurrentRefinements clearsQuery={true} />
--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -1,18 +1,18 @@
 import React from "react";
-import { Navbar } from "react-bootstrap";
-import NavbarCollapse from "react-bootstrap/esm/NavbarCollapse";
-import { Nav } from "react-bootstrap";
-
 
 const NavBar = () => {
   return (
-    <div id="homebar">
-      <Navbar>
-        <Nav.Link href="/">Aerospace Market Landscape</Nav.Link>
-        <NavbarCollapse className='justify-content-center'></NavbarCollapse>
-        <Nav.Link href='https://www.surveymonkey.com/r/25G9H5K'>mailing list</Nav.Link>
-      </Navbar>
-    </div>
+    <table id="homebar">
+      <tr>
+        <td>
+          <a href="/">Machina: Industrials Search</a>
+        </td>
+        <td style={{ 'textAlign': 'right' }}>
+          <a href='https://www.surveymonkey.com/r/25G9H5K'>mailing list</a>
+        </td>
+      </tr>
+      <tr id="spacer"/>
+    </table>
   );
 };
 
